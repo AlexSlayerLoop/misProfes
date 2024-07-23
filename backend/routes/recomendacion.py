851de2f576e87f902094185d8df22bc895f75be1
Julio Estrada <juliojr281001@gmail.com>
2024-07-22 20:50:55 -0600

    chore: crear ruta para crear una recomendacion de un profesor

--- backend/routes/recomendacion.py
+++ backend/routes/recomendacion.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from models import Recomendacion as RecomendacionModel
-from schemas.recomendacion import Recomendacion
+from schemas.recomendacion import Recomendacion, RecomendacionCreate
 from models import Profesor
 from db import SessionLocal
 
@@ -19,9 +19,20 @@ def get_db():
         
         
 @router.get("/profesor/{profesor_id}/recomendaciones", response_model=List[Recomendacion])
-def read_recomendaciones(profesor_id: int, db: Session = Depends(get_db)):
+async def read_recomendaciones(profesor_id: int, db: Session = Depends(get_db)):
     recomendaciones = db.query(RecomendacionModel).filter(RecomendacionModel.id_profesor == profesor_id).all()
     if not recomendaciones:
         raise HTTPException(status_code=404, detail="No recommendations found for this professor")
     return recomendaciones
 
+
+@router.post("/profesor/{profesor_id}/recomendacion", response_model=Recomendacion)
+async def create_recomendacion(profesor_id: int, recomendacion: RecomendacionCreate, db: Session = Depends(get_db)):
+    db_recomendacion = RecomendacionModel(id_profesor=profesor_id, 
+                                          comentario=recomendacion.comentario,
+                                          calificacion=recomendacion.calificacion,
+                                          facilidad=recomendacion.facilidad)
+    db.add(db_recomendacion)
+    db.commit()
+    db.refresh(db_recomendacion)
+    return db_recomendacion
